d36b81eb303e9d6d6fe5bd6544f1512057d874f5
Silly_Gamer <basisty.marcin@gmail.com>
2024-05-02 14:31:46 +0200

    Update BST.cpp

--- BST/BST.cpp
+++ BST/BST.cpp
@@ -1,35 +1,5 @@
 #include "BST.h"
 
-bool BST::searchTree(Node * node, int value){
-    if(!node)
-        return false;
-    else if(node->key == value){
-        return true;
-    }
-    else if(value < node->key){
-        return searchTree(node->left, value);
-    }
-    else{
-        return searchTree(node->right, value);
-    }
-}
-
-int BST::MaxKey(){
-    Node * current = root;
-    while(current->right){
-        current = current->right;
-    }
-    return current->key;
-}
-
-int BST::MinKey(){
-    Node * current = root;
-    while(current->left){
-        current = current->left;
-    }
-    return current->key;
-}
-
 Node * createNode(int value){
     Node * node = new Node;
     node->key = value;
@@ -37,149 +7,163 @@ Node * createNode(int value){
     return node;
 }
 
-Node * BST::insertNode(Node * &node, int data){
-    if(!node){
-        node = createNode(data);
-        return node;
-    }
-    if(data < node->key)
-        node->left = insertNode(node->left, data);
-    else if (data >= node->key)
-        node->right = insertNode(node->right, data); 
-
-    return node;
+bool BST::searchTree(Node * x, int key){
+    if(!x)
+        return false;
+    else if(x->key == key)
+        return true;
+    else if(key < x->key)
+        return SEARCH(x->left, key);
+    else
+        return SEARCH(x->right, key);
 }
 
-// INSERT FUNCTION ITERATIVE APPROACH //
-// void BST::insertNode(int value){
-//     Node * newNode = createNode(value);
-//     if(!root){
-//         root = newNode;
-//     }
-//     else{
-//         Node * current = root;
-//         while(1){
-//             if(current->key <= newNode->key && !current->right){
-//                 current->right = newNode;
-//                 break;
-//             }
-//             else if(current->key <= newNode->key && current->right){
-//                 current = current->right;
-//             }
-//             else if(current->key > newNode->key && !current->left){
-//                 current->left = newNode;
-//                 break;
-//             }
-//             else{
-//                 current = current->left;
-//             }
-//         }
-//     }
-// }
+void BST::RecursiveTreeInsert(Node * z){
+    if(root == NULL)
+        root = z;
+    else
+        INSERT(NULL, root, z);
+}
 
-void BST::InOrder(Node * root, int space){
+void BST::drawTree(Node * root, int space){
     Node * current = root;
-    if(!current){
-        return;
-    }
-
-    space += count;
-
-    InOrder(current->right, space);
-
-    for(int i = count; i < space; i++)
-        std::cout << " ";
-    std::cout << current->key << '\n';
-
-    InOrder(current->left, space);  
+    if(current != NULL){
+        space += count;
+        drawTree(current->right, space);
+        for(int i = count; i < space; i++)
+            std::cout << " ";
+        std::cout << current->key << '\n';
+        drawTree(current->left, space); 
+    } 
 }
 
-void BST::InOrder(Node * root){
-    Node * current = root;
-    if(!current){
-        return;
-    }
-    InOrder(current->right);
-    std::cout << current->key << ' ';
-    InOrder(current->left);  
+int BST::Maximum(Node * x){
+    if(x->right != NULL)
+        return Maximum(x->right);
+    else 
+        return x->key;
 }
 
-void BST::PreOrder(Node * root){
-    Node * current = root;
-    if(!current){
-        return;
-    }
-    std::cout << current->key << ' ';
-    PreOrder(current->right);
-    PreOrder(current->left);  
+int BST::Minimum(Node * x){
+    if(x->left != NULL)
+        return Minimum(x->left);
+    else 
+        return x->key;
 }
 
-void BST::PostOrder(Node * root){
-    Node * current = root;
-    if(!current){
-        return;
+int BST::Predecessor(Node * x){
+    if(x->left != NULL)
+        return Maximum(x->left);
+    Node * y = x->parent;
+    while(y != NULL && x == y->left){
+        x = y;
+        y = y->parent;
     }
-    PostOrder(current->right);
-    PostOrder(current->left);  
-    std::cout << current->key << " ";
+    return y->key;
 }
 
-//  BST-Predecessor(x)
-//      if x.left ≠ NIL then
-//          return BST-Maximum(x.left)
-//      end if
-//      y := x.parent
-//      while y ≠ NIL and x = y.left do
-//          x := y
-//          y := y.parent
-//      repeat
-//      return y
-// Node * Precessor(Node * root){
+int BST::Successor(Node * x){
+    if(x->right != NULL)
+        return Minimum(x->right);
+    Node * y = x->parent;
+    while(y != NULL && x == y->right){
+        x = y;
+        y = y->parent;
+    }
+    return y->key;
+}
 
-// }
+void BST::InOrder(Node * x){
+    if(x != NULL){
+        InOrder(x->left);
+        std::cout << x->key << " ";
+        InOrder(x->right);
+    }
+}
 
-// BST-Successor(x)
-//      if x.right ≠ NIL then
-//          return BST-Minimum(x.right)
-//      end if
-//      y := x.parent
-//      while y ≠ NIL and x = y.right do
-//          x := y
-//          y := y.parent
-//      repeat
-//      return y
+void BST::PreOrder(Node * x){
+    if(x != NULL){
+        std::cout << x->key << " ";
+        PreOrder(x->left);
+        PreOrder(x->right);
+    }
+}
 
-// Node * Successor(Node * root){
+void BST::PostOrder(Node * x){
+    if(x != NULL){
+        PostOrder(x->left);
+        PostOrder(x->right);
+        std::cout << x->key << " ";
+    }
+}
 
-// }
+void BST::RightRotate(Node * y){
+    Node * x = y->left;
+    y->left = x->right;
+    if(x->right != NULL)
+        x->right->parent = y;
+    x->parent = y->parent;
+    if(y->parent == NULL)
+        root = x;
+    else if(y == y->parent->right)
+        y->parent->right = x;
+    else 
+        y->parent->left = x;
+    x->right = y;
+    y->parent = x;
+}
+   
+void BST::LeftRotate(Node * x){
+    Node * y = x->right;
+    x->right = y->left;
+    if(y->left != NULL)
+        y->left->parent = x;
+    y->parent = x->parent;
+    if(x->parent == NULL)
+        root = y;
+    else if(x == x->parent->right)
+        x->parent->left = y;
+    else 
+        x->parent->right = y;
+    y->left = x;
+    x->parent = y;
+}
 
-// 1    BST-Delete(BST, D)
-// 2      if D.left = NIL then
-// 3        Shift-Nodes(BST, D, D.right)
-// 4      else if D.right = NIL then
-// 5        Shift-Nodes(BST, D, D.left)
-// 6      else
-// 7        E := BST-Successor(D)
-// 8        if E.parent ≠ D then
-// 9          Shift-Nodes(BST, E, E.right)
-// 10         E.right := D.right
-// 11         E.right.parent := E
-// 12       end if
-// 13       Shift-Nodes(BST, D, E)
-// 14       E.left := D.left
-// 15       E.left.parent := E
-// 16     end if
+void BST::INSERT(Node * p, Node * x, Node * z){
+    if(x == NULL){
+        z->parent = p;
+        if(z->key < p->key)
+            p->left = z;
+        else
+            p->right = z;
+    }
+    else if(z->key < x->key)
+        INSERT(x, x->left, z);
+    else
+        INSERT(x, x->right, z);
+}
 
+Node * BST::SEARCH(Node * x, int key){
+    if(!x)
+        return NULL;
+    else if(x->key == key)
+        return x;
+    else if(key < x->key)
+        return SEARCH(x->left, key);
+    else
+        return SEARCH(x->right, key);
+}
 
+Node * BST::MAXIMUM(Node * x){
+    if(x->right != NULL)
+        return MAXIMUM(x->right);
+    else 
+        return x;
+}
 
-// 1    Shift-Nodes(BST, u, v)
-// 2      if u.parent = NIL then
-// 3        BST.root := v
-// 4      else if u = u.parent.left then
-// 5        u.parent.left := v
-// 5      else
-// 6        u.parent.right := v
-// 7      end if
-// 8      if v ≠ NIL then
-// 9        v.parent := u.parent
-// 10     end if
+Node * BST::MINIMUM(Node * x){
+    if(x->left != NULL)
+        return MAXIMUM(x->left);
+    else 
+        return x;
+}
