f86c04cb7f4fc1cdb85b4ad12de9c98960136fe6
Silly_Gamer <basisty.marcin@gmail.com>
2024-05-16 10:20:31 +0200

    Delete src directory

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "GameManager.h"
-
-int main(int  argc, char * argv[]){
-    GameManager * Game = GameManager::Instance();
-    Game->Run();
-
-    GameManager::Release();
-    Game = NULL;
-}
